60fceaa33804702e764c75afa2979c0b4d08520b
Caroline Clara de Souza <carolineclaradsl@gmail.com>
2021-12-03 19:56:47 -0300

    Altera import

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // import router from '@/router'
-import store from '@/store'
 
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import '@mdi/font/css/materialdesignicons.css'
+import store from "./store";
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
